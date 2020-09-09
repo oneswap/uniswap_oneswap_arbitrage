d87abf5e72903adf7ded254148fe78859d7638dd
oneswapdev <developer@oneswap.net>
2020-09-09 19:50:00 +0800

    Add eth support

--- contracts/FlashSwap.sol
+++ contracts/FlashSwap.sol
@@ -6,32 +6,50 @@ import './interfaces/IOneswapPair.sol';
 import './interfaces/IERC20.sol';
 import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IOneSwapFactory.sol';
+import './interfaces/IWETH.sol';
 
+struct PairInfo{
+    address token0;
+    address token1;
+    address token0ForOneswap;
+    address token1ForOneswap;
+    bool token0IsStock;
+    bool isOnlySwap;
+
+}
 contract FlashSwap {
     address public immutable uniswapFactory;
     address public immutable oneswapFactory;
+    address public immutable weth;
 
-    event AmountReturn(uint256);
-    constructor(address _factory,  address _oneswapFactory) public {
+    event AmountReturn(address,address,uint256);
+    constructor(address _factory,  address _oneswapFactory, address _weth) public {
         uniswapFactory = _factory;
         oneswapFactory = _oneswapFactory;
+        weth = _weth;
     }
 
-    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external {
+    receive() external payable { }
+    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external{
 
-        address token0 = IUniswapV2Pair(msg.sender).token0();
-        address token1 = IUniswapV2Pair(msg.sender).token1();
-        address uniswapPair = IUniswapV2Factory(uniswapFactory).getPair(token0,token1);
+        PairInfo memory pairInfo;
+        pairInfo.token0 = IUniswapV2Pair(msg.sender).token0();
+        pairInfo.token1 = IUniswapV2Pair(msg.sender).token1();
+        pairInfo.token0ForOneswap = (pairInfo.token0 == weth) ? address(0) : pairInfo.token0;
+        pairInfo.token1ForOneswap = (pairInfo.token1 == weth) ? address(0) : pairInfo.token1;
+
+        address uniswapPair = IUniswapV2Factory(uniswapFactory).getPair(pairInfo.token0,pairInfo.token1);
         {
             assert(msg.sender == uniswapPair);
-            require(amount0 == 0 || amount1 == 0,'one of token amount should be zero'); // this strategy is unidirectional
+            require(amount0 == 0 || amount1 == 0,'Either amount0 or amount1 should be zero'); // this strategy is unidirectional
         }
+
         address pairAddress;
         {
-            (bool token0IsStock, bool isOnlySwap) = abi.decode(data,(bool,bool));
-            address stock = token0IsStock ? token0: token1;
-            address money = token0IsStock ? token1: token0;
-            pairAddress = IOneSwapFactory(oneswapFactory).tokensToPair(stock,money,isOnlySwap);
+            (pairInfo.token0IsStock, pairInfo.isOnlySwap) = abi.decode(data,(bool,bool));
+            address stock = pairInfo.token0IsStock ? pairInfo.token0ForOneswap: pairInfo.token1ForOneswap;
+            address money = pairInfo.token0IsStock ? pairInfo.token1ForOneswap: pairInfo.token0ForOneswap;
+            pairAddress = IOneSwapFactory(oneswapFactory).tokensToPair(stock,money,pairInfo.isOnlySwap);
             require(pairAddress != address(0), 'OneSwap Pair does not exist!');
         }
 
@@ -39,21 +57,50 @@ contract FlashSwap {
         (uint reserve0,uint reserve1,) = IUniswapV2Pair(uniswapPair).getReserves();
 
         if (amount0 > 0) {
-            IERC20(token0).transfer(pairAddress,amount0);
-            uint amountReceived = pair.addMarketOrder(token0,address(this),uint112(amount0));
-            uint amountRequired = UniswapV2Library.getAmountIn(amount0, reserve1, reserve0);
-            require(amountReceived > amountRequired,'No profit to earn');
-            IERC20(token1).transfer(msg.sender,amountRequired);
-            IERC20(token1).transfer(sender,amountReceived - amountRequired);
-            emit AmountReturn(amountReceived - amountRequired);
+                _safeTransferWETHToETH(pairInfo.token0, pairAddress, amount0);
+                uint amountReceived = pair.addMarketOrder(pairInfo.token0ForOneswap,address(this),uint112(amount0));
+                uint amountRequired = UniswapV2Library.getAmountIn(amount0, reserve1, reserve0);
+                require(amountReceived > amountRequired,'No profit to earn');
+                _safeTransferETHToWETH(pairInfo.token1,msg.sender,amountRequired);
+                _safeTransfer(pairInfo.token1, sender,amountReceived - amountRequired);
+                emit AmountReturn(pairInfo.token1, sender, amountReceived - amountRequired);
         } else {
-            IERC20(token1).transfer(pairAddress,amount1);
-            uint amountReceived = pair.addMarketOrder(token1,address(this),uint112(amount1));
-            uint amountRequired = UniswapV2Library.getAmountIn(amount1, reserve0, reserve1);
-            require(amountReceived > amountRequired,'No profit to earn');
-            IERC20(token0).transfer(msg.sender,amountRequired);
-            IERC20(token0).transfer(sender,amountReceived - amountRequired);
-            emit AmountReturn(amountReceived - amountRequired);
+                _safeTransferWETHToETH(pairInfo.token1, pairAddress, amount1);
+                uint amountReceived = pair.addMarketOrder(pairInfo.token1ForOneswap,address(this),uint112(amount1));
+                uint amountRequired = UniswapV2Library.getAmountIn(amount1, reserve0, reserve1);
+                require(amountReceived > amountRequired,'No profit to earn');
+                _safeTransferETHToWETH(pairInfo.token0,msg.sender,amountRequired);
+                _safeTransfer(pairInfo.token0, sender, amountReceived - amountRequired);
+                emit AmountReturn(pairInfo.token0, sender, amountReceived - amountRequired);
+
         }
     }
+    function _safeTransferWETHToETH(address token, address to, uint amount) internal {
+        if (token == weth){
+            IWETH(weth).withdraw(amount);
+            _safeTransferETH(to,amount);
+        }else{
+            require(IERC20(token).transfer(to,amount),'ERC20 transfer failed');
+        }
+    }
+    function _safeTransferETHToWETH(address token, address to, uint amount) internal {
+        if (token == weth){
+            IWETH(weth).deposit{value:amount}();
+            require(IWETH(weth).transfer(to, amount),'WETH transfer failed');
+        }else{
+            require(IERC20(token).transfer(to,amount),'ERC20 transfer failed');
+        }
+    }
+    function _safeTransfer(address token, address to, uint amount) internal {
+        if (token == weth){
+            _safeTransferETH(to, amount);
+        }else{
+            require(IERC20(token).transfer(to,amount),'ERC20 transfer failed');
+        }
+    }
+    function _safeTransferETH(address to, uint value) internal {
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, 'ETH transfer failed');
+    }
+
 }
--- test/FlashSwapTest.js
+++ test/FlashSwapTest.js
@@ -4,11 +4,13 @@ const oneswapFactory = artifacts.require("OneSwapFactoryPXYTEST");
 const Ones = artifacts.require("OneSwapToken");
 const pairCon = artifacts.require("OneSwapPair");
 const FlashSwap = artifacts.require("FlashSwap");
+const WETH9 = artifacts.require("WETH9");
 
 contract("test flash swap with oneswap", async (accounts)=>{
     before(async()=>{
         uniswap = await uniswapFactory.deployed();
         oneswap = await oneswapFactory.deployed();
+        weth = await WETH9.deployed();
         ones = await Ones.new("ones", "ones", 100_0000_0000, 18);
         usdt = await Ones.new("usdt", "usdt", 100_0000_0000, 18);
         console.log('ones address:',ones.address);
@@ -56,7 +58,7 @@ contract("test flash swap with oneswap", async (accounts)=>{
    });
 
    it("create flashswap contracts",async()=>{
-        flashSwap = await FlashSwap.new(uniswap.address,oneswap.address);
+        flashSwap = await FlashSwap.new(uniswap.address,oneswap.address,weth.address);
         console.log(flashSwap.address);
    });
 
@@ -78,5 +80,161 @@ contract("test flash swap with oneswap", async (accounts)=>{
        balance = await usdt.balanceOf(accounts[1]);
        console.log(balance);
    })
+
+})
+
+contract("test flash swap with oneswap of eth pair", async (accounts)=>{
+    before(async()=>{
+        uniswap = await uniswapFactory.deployed();
+        oneswap = await oneswapFactory.deployed();
+        weth = await WETH9.deployed();
+        ones = await Ones.new("ones", "ones", 100_0000_0000, 18);
+        console.log('ones address:',ones.address);
+        console.log('weth address:',weth.address);
+    });
+
+    it("create uniswap pair & add liquidity",async () =>{
+        result = await uniswap.createPair(ones.address,weth.address);
+        uniswapp = await uniswapPair.at(result.logs[0].args[2]);
+        console.log('uniswap pair address:',uniswapp.address);
+        let onesToAdd = 10000;
+        await ones.transfer(uniswapp.address,onesToAdd);
+        await weth.deposit({value:100*onesToAdd});
+        await weth.transfer(uniswapp.address,100*onesToAdd);
+        await uniswapp.mint(accounts[1]);
+        reserves = await uniswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+        onesIsToken0 = false;
+        if (ones.address<weth.address){
+            onesIsToken0 = true;
+        }
+        if (onesIsToken0){
+            assert.equal(reserves[0].toNumber(),onesToAdd);
+            assert.equal(reserves[1].toNumber(),100*onesToAdd);
+        }else{
+            assert.equal(reserves[1].toNumber(),onesToAdd);
+            assert.equal(reserves[0].toNumber(),100*onesToAdd);
+        }
+    });
+
+    it("create oneswap pair & add liquidity", async ()=>{
+        impl = await pairCon.new();
+        result = await oneswap.createPair(ones.address, '0x0000000000000000000000000000000000000000', impl.address);
+        pairAddr = result.logs[0].args.pair;
+        console.log("oneswap pair address: ", pairAddr)
+        oneswapp = await pairCon.at(pairAddr)
+
+        let onesToAdd = 10000;
+        await ones.transfer(oneswapp.address,onesToAdd);
+        await web3.eth.sendTransaction({from:accounts[0],to:oneswapp.address,value:400*onesToAdd});
+        await oneswapp.mint(accounts[1]);
+        reserves = await oneswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+        assert.equal(reserves[0].toNumber(),onesToAdd);
+        assert.equal(reserves[1].toNumber(),400*onesToAdd);
+    });
+
+    it("create flashswap contracts",async()=>{
+        flashSwap = await FlashSwap.new(uniswap.address,oneswap.address,weth.address);
+        console.log(flashSwap.address);
+    });
+
+    it("flashswap with oneswap", async ()=>{
+        reserves = await uniswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+
+        balance = await web3.eth.getBalance(accounts[1]);
+        console.log(balance);
+
+        bytes = web3.eth.abi.encodeParameters(['bool','bool'],[onesIsToken0,false]);
+        if (onesIsToken0){
+            result = await uniswapp.swap(100,0,flashSwap.address,bytes,{from:accounts[1]});
+            console.log(result.logs)
+        }else{
+            await uniswapp.swap(0,100,flashSwap.address,bytes,{from:accounts[1]});
+        }
+        reserves = await uniswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+        balance = await web3.eth.getBalance(accounts[1]);
+        console.log(balance);
+    })
+
+})
+
+contract("test flash swap with oneswap of eth pair", async (accounts)=>{
+    before(async()=>{
+        uniswap = await uniswapFactory.deployed();
+        oneswap = await oneswapFactory.deployed();
+        weth = await WETH9.deployed();
+        ones = await Ones.new("ones", "ones", 100_0000_0000, 18);
+        console.log('ones address:',ones.address);
+        console.log('weth address:',weth.address);
+    });
+
+    it("create uniswap pair & add liquidity",async () =>{
+        result = await uniswap.createPair(ones.address,weth.address);
+        uniswapp = await uniswapPair.at(result.logs[0].args[2]);
+        console.log('uniswap pair address:',uniswapp.address);
+        let onesToAdd = 10000;
+        await ones.transfer(uniswapp.address,onesToAdd);
+        await weth.deposit({value:400*onesToAdd});
+        await weth.transfer(uniswapp.address,400*onesToAdd);
+        await uniswapp.mint(accounts[1]);
+        reserves = await uniswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+        onesIsToken0 = false;
+        if (ones.address<weth.address){
+            onesIsToken0 = true;
+        }
+        if (onesIsToken0){
+            assert.equal(reserves[0].toNumber(),onesToAdd);
+            assert.equal(reserves[1].toNumber(),400*onesToAdd);
+        }else{
+            assert.equal(reserves[1].toNumber(),onesToAdd);
+            assert.equal(reserves[0].toNumber(),400*onesToAdd);
+        }
+    });
+
+    it("create oneswap pair & add liquidity", async ()=>{
+        impl = await pairCon.new();
+        result = await oneswap.createPair(ones.address, '0x0000000000000000000000000000000000000000', impl.address);
+        pairAddr = result.logs[0].args.pair;
+        console.log("oneswap pair address: ", pairAddr)
+        oneswapp = await pairCon.at(pairAddr)
+
+        let onesToAdd = 10000;
+        await ones.transfer(oneswapp.address,onesToAdd);
+        await web3.eth.sendTransaction({from:accounts[0],to:oneswapp.address,value:100*onesToAdd});
+        await oneswapp.mint(accounts[1]);
+        reserves = await oneswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+        assert.equal(reserves[0].toNumber(),onesToAdd);
+        assert.equal(reserves[1].toNumber(),100*onesToAdd);
+    });
+
+    it("create flashswap contracts",async()=>{
+        flashSwap = await FlashSwap.new(uniswap.address,oneswap.address,weth.address);
+        console.log(flashSwap.address);
+    });
+
+    it("flashswap with oneswap", async ()=>{
+        reserves = await uniswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+
+        balance = await ones.balanceOf(accounts[1]);
+        console.log(balance);
+
+        bytes = web3.eth.abi.encodeParameters(['bool','bool'],[onesIsToken0,false]);
+        if (onesIsToken0){
+            result = await uniswapp.swap(0,400,flashSwap.address,bytes,{from:accounts[1]});
+            console.log(result.logs)
+        }else{
+            await uniswapp.swap(400,0,flashSwap.address,bytes,{from:accounts[1]});
+        }
+        reserves = await uniswapp.getReserves();
+        console.log(reserves[0],reserves[1]);
+        balance = await ones.balanceOf(accounts[1]);
+        console.log(balance);
+    })
 
 })
